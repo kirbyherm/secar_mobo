7bf4de7deb4317cd54baa00e89ef3871783c0007
kirbyherm <khermans21@gmail.com>
2021-10-21 16:44:04 -0400

    fixing issue with additional colored lines in cosy, adding kmeans files

--- py/cosy_draw.sh
+++ py/cosy_draw.sh
@@ -9,7 +9,7 @@
 #
 # echo -e "version\tn\tmax\tl2norm\ttime" 
 
-for TOSSES in $(seq 0 1 5) 
+for TOSSES in $(seq 0 1 15) 
 do
 #  ./draw.py ../../output/output_4f_moead_FP2_FP3_150_"$TOSSES".csv
   cd 4f_FP2_FP3
